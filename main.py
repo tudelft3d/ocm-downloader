a866ef091c1df658712fdd370e69594c0a853697
Stelios Vitalis <steliosvitalis@gmail.com>
2019-01-24 14:23:17 +0100

    Initial commit

---
+++ main.py
@@ -0,0 +1,10 @@
+import boto3
+from botocore.handlers import disable_signing
+
+resource = boto3.resource('s3')
+resource.meta.client.meta.events.register('choose-signer.s3.*', disable_signing)
+
+bucket = resource.Bucket('opencitymodel')
+
+for file in bucket.objects.all():
+    print(file)
