46312cc12dc6837c18bee37fa10abed9fbdd4540
Stelios Vitalis <steliosvitalis@gmail.com>
2019-01-24 16:59:18 +0100

    Implement the actual downloading of files

--- main.py
+++ main.py
@@ -1,5 +1,6 @@
 import boto3
 from botocore.handlers import disable_signing
+import ntpath
 
 resource = boto3.resource('s3')
 resource.meta.client.meta.events.register('choose-signer.s3.*', disable_signing)
@@ -7,4 +8,7 @@ resource.meta.client.meta.events.register('choose-signer.s3.*', disable_signing)
 bucket = resource.Bucket('opencitymodel')
 
 for file in bucket.objects.all():
-    print(file)
+    if file.key.endswith('.json'):
+        local_filename = './' + ntpath.basename(file.key)
+        bucket.download_file(file.key, local_filename)
+        print('Downloaded ' + file.key)
