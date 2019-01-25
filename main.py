defeb8852aa944c6bc99e4e5052b58f2cc4e4720
Stelios Vitalis <steliosvitalis@gmail.com>
2019-01-25 15:34:25 +0100

    Add support for argument (format and area-name)
    This fixes #1 and #2

--- main.py
+++ main.py
@@ -1,6 +1,30 @@
 import boto3
 from botocore.handlers import disable_signing
 import ntpath
+import sys
+
+# Setup the default configuration
+format = '.json'
+area = ''
+
+# Read the arguments
+i = 1
+while len(sys.argv) > i:
+    if sys.argv[i] == "--format":
+        if sys.argv[i + 1] == "CityJSON":
+            format = ".json"
+        elif sys.argv[i + 1] == "CityGML":
+            format = ".zip"
+        else:
+            print("The format provided is not supported (available options are CityJSON/CityGML)")
+        i = i + 2
+    elif sys.argv[i] == "--area-name":
+        area = sys.argv[i + 1]
+        i = i + 2
+    else:
+        print("Option " + sys.argv[i] + " not supported")
+        i = i + 1
+
 
 resource = boto3.resource('s3')
 resource.meta.client.meta.events.register('choose-signer.s3.*', disable_signing)
@@ -8,7 +32,8 @@ resource.meta.client.meta.events.register('choose-signer.s3.*', disable_signing)
 bucket = resource.Bucket('opencitymodel')
 
 for file in bucket.objects.all():
-    if file.key.endswith('.json'):
-        local_filename = './' + ntpath.basename(file.key)
-        bucket.download_file(file.key, local_filename)
-        print('Downloaded ' + file.key)
+    if file.key.endswith(format):
+        if area in file.key or area == '':
+            local_filename = './' + ntpath.basename(file.key)
+            bucket.download_file(file.key, local_filename)
+            print('Downloaded ' + file.key)
